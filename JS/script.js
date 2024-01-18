69ba24d9be49ee83e7422ab3d94924ecb832a393
mstrpyyy <aryahanifn@gmail.com>
2024-01-18 13:38:03 +0700

    final 1

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,16 @@
 function changeCalculationType() {
     var selectElement = document.getElementById("hitung");
-    var resultLabel = document.getElementById("resultLabel");
     var resultLabel2 = document.getElementById("resultLabel2");
     var resultLabel3 = document.getElementById("resultLabel3");
     var resultLabel4 = document.getElementById("resultLabel4");
     var resultLabel5 = document.getElementById("resultLabel5");
 
     if (selectElement.value === "luas") {
-        // resultLabel.textContent = "Luas Persegi";
         resultLabel2.textContent = "Rumus Luas Persegi";
         resultLabel3.textContent = "L = S x S";
         resultLabel4.textContent = "L : Luas";
         resultLabel5.textContent = "Hitung Luas Persegi";
     } else if (selectElement.value === "keliling") {
-        // resultLabel.textContent = "Keliling Persegi";
         resultLabel2.textContent = "Rumus Keliling Persegi";
         resultLabel3.textContent = "K = 4 x S";
         resultLabel4.textContent = "K : Keliling";
@@ -25,7 +22,7 @@ function changeCalculationType() {
 }
 
 function reset() {
-    document.getElementById("squareside").value = "";gi
+    document.getElementById("squareside").value = "";
     var resultLabel6 = document.getElementById("resultLabel6");
     resultLabel6.innerHTML = "";
 }
